99c091ffadcbfe150508243855b6d98012989fc5
Ryan Snyder <ryansnyder.me@gmail.com>
2023-01-01 17:35:59 +0000

    Update fg.js

--- js/fg.js
+++ js/fg.js
@@ -1,11 +1,13 @@
 // Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-94393-1', 'foodgeeks.com');
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
+(function(){
+    var script_tag = document.createElement('script');
+    script_tag.src='https://www.googletagmanager.com/gtag/js?id=G-083TM90LBK';
+    document.head.appendChild(script_tag);
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-083TM90LBK');
+})();
 
 $(window).load(function() {
   // auto focus on food journal entry area
